9cb5f87c9784940eb2c43bb0b56102bba4e21a3a
Sourav Maji <s722143@gmail.com>
2023-10-26 15:13:43 +0000

    vite app file structure

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
-
+import Chai from "./chai"
 
 function App() {
   
 
   return (
-   <h1>Chai aur react with Vite | Sourav</h1>
+  //  <h1>Chai aur react with Vite | Sourav</h1>
+  <Chai />
   )
 }
 
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,10 @@
+function Chai(){
+    return(
+        <>
+        <h3>Chai is ready</h3>
+        <p>Test Paragraph</p>
+        </>
+    )
+}
+
+export default Chai
