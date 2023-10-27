06549f9f52557c84fb0a5c374aa6e38d888c2f09
sourav-maji <s722143@gmail.com>
2023-10-27 08:27:34 +0530

    custom jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <>
+    <h1>Custom App!!</h1>
+    </>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <MyApp />
   </React.StrictMode>,
 )
