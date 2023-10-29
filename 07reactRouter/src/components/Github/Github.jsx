3c3517b4aec0ccd7add9e14173fdd0d138136e58
sourav-maji <s722143@gmail.com>
2023-10-29 09:32:43 +0530

    part 6

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState ,useEffect } from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+function Github() {
+
+    // const [data , setData] = useState([])
+
+    // useEffect(() => {
+    //   fetch("https://api.github.com/users/sourav-maji")
+    //   .then(response => response.json())
+    //   .then(data =>{
+    //     console.log(data);
+    //     setData(data)
+    //   })
+    // }, [])
+
+    const data = useLoaderData()
+    
+  return (
+    <>
+    <div className="text-center m-4 bg-gray-600  text-white p-4 text-3xl">Github followers : {data?.followers} 
+    <img src={data.avatar_url} alt="Git Picture" width={300} />
+    </div>
+    <div className="text-center m-4 bg-gray-600  text-white p-4 text-3xl">Github Repos : {data?.public_repos} </div>
+    
+    </>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async() =>{
+    const response = await fetch("https://api.github.com/users/sourav-maji")
+    return response.json()
+}
